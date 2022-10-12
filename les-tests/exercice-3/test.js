5f8b6c3203c820bc7bd91bc20932862d1d6c4f9d
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-12 14:32:36 +0200

    Test

---
+++ les-tests/exercice-3/test.js
@@ -0,0 +1,12 @@
+// Avec comme paramètres 1 et 2,
+// la fonction somme() est sensée retourner 3.
+
+
+
+// Avec uniquement 2 en paramètre,
+// la fonction somme() est sensée retourner 2.
+
+
+
+// Avec comme paramètres 'un' et 2,
+// la fonction somme() est sensée retourner l'erreur "paramètre invalide".
