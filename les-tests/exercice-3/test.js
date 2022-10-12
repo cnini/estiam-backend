97bf46b58fc9fc963b1cbbcb9e872dceff2b6760
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-12 14:49:56 +0200

    Étape 2 : fait

--- les-tests/exercice-3/test.js
+++ les-tests/exercice-3/test.js
@@ -1,12 +1,28 @@
-// Avec comme paramètres 1 et 2,
-// la fonction somme() est sensée retourner 3.
+const expect = require('expect.js')
+
+describe("La fonction somme() est sensée retourner", () => {
+
+    // Avec comme paramètres 1 et 2,
+    // la fonction somme() est sensée retourner 3.
+    it("3 si comme paramètres on met 1 et 2.", async () => {
+        const resultat = await somme(1,2)
+        expect(resultat).to.equal(3)
+    })
 
 
+    // Avec uniquement 2 en paramètre,
+    // la fonction somme() est sensée retourner 2.
+    it("2 si comme paramètres, on met uniquement 2.", async () => {
+        const resultat = await somme(2)
+        expect(resultat).to.equal(2)
+    })
 
-// Avec uniquement 2 en paramètre,
-// la fonction somme() est sensée retourner 2.
 
+    // Avec comme paramètres 'un' et 2,
+    // la fonction somme() est sensée retourner l'erreur "paramètre invalide".
+    it("l'erreur \"Paramètre invalide\" si comme paramètres, on met 'un' et 2.", async () => {
+        const resultat = await somme('un', 2)
+        expect(resultat).to.equal("Paramètre invalide")
+    })
 
-
-// Avec comme paramètres 'un' et 2,
-// la fonction somme() est sensée retourner l'erreur "paramètre invalide".
+})
