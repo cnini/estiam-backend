f408f83fc7ffc80ab0e046f79c86c68b84b366e2
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-12 14:56:25 +0200

    Étapes 4 et 5 : faits

--- les-tests/exercice-3/test.js
+++ les-tests/exercice-3/test.js
@@ -4,15 +4,17 @@ const expect = require('expect.js')
 // Tout paramètre manquant aura 0 comme valeur par défaut.
 // Si un des paramètre n'est pas de type `number`, la fonction lèvera une
 // exception avec le message d'erreur "paramètre invalide".
-function somme(nombre1, nombre2) {
-    // TODO
+function somme(nombre1 = 0, nombre2 = 0) {
+    if(typeof nombre1 !== 'number' || typeof nombre2 !== 'number') return "Paramètre invalide";
+
+    return nombre1 + nombre2
 }
 
 describe("La fonction somme() est sensée retourner", () => {
 
     // Avec comme paramètres 1 et 2,
     // la fonction somme() est sensée retourner 3.
-    it("3 si comme paramètres on met 1 et 2.", async () => {
+    it("3 si comme paramètres on met 1 et 2 ", async () => {
         const resultat = await somme(1,2)
         expect(resultat).to.equal(3)
     })
@@ -20,7 +22,7 @@ describe("La fonction somme() est sensée retourner", () => {
 
     // Avec uniquement 2 en paramètre,
     // la fonction somme() est sensée retourner 2.
-    it("2 si comme paramètres, on met uniquement 2.", async () => {
+    it("2 si comme paramètres, on met uniquement 2 ", async () => {
         const resultat = await somme(2)
         expect(resultat).to.equal(2)
     })
@@ -28,7 +30,7 @@ describe("La fonction somme() est sensée retourner", () => {
 
     // Avec comme paramètres 'un' et 2,
     // la fonction somme() est sensée retourner l'erreur "paramètre invalide".
-    it("l'erreur \"Paramètre invalide\" si comme paramètres, on met 'un' et 2.", async () => {
+    it("l'erreur \"Paramètre invalide\" si comme paramètres, on met 'un' et 2 ", async () => {
         const resultat = await somme('un', 2)
         expect(resultat).to.equal("Paramètre invalide")
     })
